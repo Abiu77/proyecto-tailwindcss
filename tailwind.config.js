109cfb1ee18625a96bd3bebc855e832c0ee5adac
ABIU Condori <elicondoriquispe860@gmail.com>
2023-10-15 23:00:23 -0500

    Personalizacion de fuentes y colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
       fontFamily:{
         'sans' : "'Work Sans', sans-serif",
         'cascadia': '"CascadiaCode"',
+      },
+      colors:{
+        'do-blue-dark': '#080c2d',
+        'do-blue-medium': 'rgb(20, 86, 255)',
+        'do-blue-light': 'rgb(0, 105, 255)',
       }
     },
   },
